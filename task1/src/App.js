91b8dede342bce4eeae9e6942a8b85764f3af520
Shivaprasadgunaganti <shivaprasad06559@gmail.com>
2024-09-16 17:19:03 +0530

    update

--- task1/src/App.js
+++ task1/src/App.js
@@ -8,12 +8,3 @@ function App() {
   );
 }
 export default App;
-
-// import './App.css';
-// import { Data } from './components/todosData/data';
-// function App() {
-//   return (
-//     <div className="App">
-//      <Data/>
-//     </div> ); }
-// export default App;
